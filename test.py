85f376d5d3d19b7293e65ed4c3bcd588b2e95630
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2016-02-10 21:10:40 +0100

    small start of proper test file

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
 import sys
 from xmeml import VideoSequence
+from xmeml.iter import XmemlParser
 
-v1 = VideoSequence(file=sys.argv[1])
-for z in [v for v in v1.track_items if v.track.type == "audio"]:
-    print z.name, z.audibleframes()
+def test_read(f):
+    'Read file'
+    x = XmemlParser(f)
+    assert x
 
+
+def test_audibleranges(f):
+    x = XmemlParser(f)
+    for cl in x.iteraudioclips(onlypureaudio=True):
+        cl.audibleframes()
+
+
+
+"""
+  v1 = VideoSequence(file=sys.argv[1])
+  for z in [v for v in v1.track_items if v.track.type == "audio"]:
+      print z.name, z.audibleframes()
+
+"""
+
+if __name__ == '__main__':
+    test_read(sys.argv[1])
+    test_audibleranges(sys.argv[1])
